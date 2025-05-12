bc5e26362f65dcdb2034e60c79b94ab63416f187
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-12 22:12:13 +0400

    feat: add create comment dto

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
+export class CreateCommentDto {
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\d{2}\/\d{2}\/\d{4}$/, {
+    message: 'createdAt must be in the format DD/MM/YYYY',
+  })
+  createdAt: string;
+}
