d13124eff561e597bb4f466f605efd4eccecd782
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-12 20:49:48 +0400

    feat: add validations for create task

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,31 @@
-export class CreateTaskDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Matches,
+} from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+  @IsString()
+  @IsOptional()
+  description: string;
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\d{2}\/\d{2}\/\d{4}$/, {
+    message: 'dueDate must be in the format DD/MM/YYYY',
+  })
+  dueDate: string;
+  @IsNumber()
+  @IsNotEmpty()
+  statusId: number;
+  @IsNumber()
+  @IsNotEmpty()
+  asigneeId: number;
+  @IsNumber()
+  @IsNotEmpty()
+  createdById: number;
+}
