b664dbdb95ead2ef382b5d005f7e16ef914af8be
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-12 21:21:28 +0400

    feat: add task update dto

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,4 +1,27 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { IsNumber, IsOptional, IsString, Matches } from 'class-validator';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsString()
+  @IsOptional()
+  title: string;
+  @IsString()
+  @IsOptional()
+  description: string;
+  @IsString()
+  @IsOptional()
+  @Matches(/^\d{2}\/\d{2}\/\d{4}$/, {
+    message: 'dueDate must be in the format DD/MM/YYYY',
+  })
+  dueDate: string;
+  @IsNumber()
+  @IsOptional()
+  statusId: number;
+  @IsNumber()
+  @IsOptional()
+  asigneeId: number;
+  @IsNumber()
+  @IsOptional()
+  createdById: number;
+}
