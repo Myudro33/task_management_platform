4813b815f9017696a5cbc893588c00f35b3f620f
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-12 20:50:01 +0400

    feat: add create task function

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 
-@Controller('task')
+@Controller('/api/tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
@@ -17,12 +25,7 @@ export class TaskController {
     return this.taskService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.taskService.findOne(+id);
-  }
-
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.taskService.update(+id, updateTaskDto);
   }
